eed992ecaebb9a972339021ba03d803e3d711ef5
Mundia-Pizo <stacypizom@gmail.com>
2022-10-25 08:04:51 -0700

    some changes for you

--- pages/post.js
+++ pages/post.js
@@ -1,17 +1,14 @@
 import React from 'react'
 import 'react-quill/dist/quill.snow.css'
 import dynamic from "next/dynamic";
-import homeStyles from '../styles/Home.module.css'
 
-const createPost = () => {
+const posts = () => {
   return (
-    <div className={homeStyles.rich_text}>
-      <QuillNoSSRWrapper theme="snow" />
-    </div>
+    <QuillNoSSRWrapper  theme="snow" />
   )
 }
 
-export default createPost
+export default posts
 
 const QuillNoSSRWrapper = dynamic(import('react-quill'), {	
 	ssr: false,
